c9a5c154db41d7e446febc329c34c3bece5c33ee
Brayan Bautista <brayan.bautista@megaterios.co>
2019-04-17 13:59:47 -0500

    subida a heroku cambio de Encabezado HTTP_HOST

--- business/settings.py
+++ business/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zy^)9*z5ent=6^d@lqd%9d6c8b)r)ui4i!adk!6oexrim-m%jh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['freenet-business.herokuapp.com']
 
 
 # Application definition
