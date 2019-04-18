43505f7fa21c52cbf15492788adddb66f74f5037
Brayan <ganbetacool@gmail.com>
2019-04-17 23:28:39 -0500

    Heroku, se agredo envio de correo de contacto a soporte

--- apps/pagina/views.py
+++ apps/pagina/views.py
@@ -28,17 +28,17 @@ def contact(request):
 	if request.method == "POST":
 		statement_services_all = Service.objects.all()
 		statement = Contact(name=request.POST.get('name'), message=request.POST.get('message'), email=request.POST.get('email'),)
-		#statement.save()
+		statement.save()
 
 		email = request.POST.get('email', None)
 		message = request.POST.get('message')
 		name = request.POST.get('name')
 		context = {'email': email, 'message': message, 'name':name, 'enviado':'enviado con exito', 'statement_services_all': statement_services_all}
 
-		html_message = render_to_string('template_email/email.html', {'context': context})
+		#html_message = render_to_string('template_email/email.html', {'context': context})
 		plain_message = strip_tags(html_message)
 		email_soport = 'ingjavierbuitrago@gmail.com'
-		send_mail('solicitud de contacto', plain_message, settings.EMAIL_HOST_USER,[email_soport], html_message=html_message, fail_silently=False)
+		#send_mail('solicitud de contacto', plain_message, settings.EMAIL_HOST_USER,[email_soport], html_message=html_message, fail_silently=False)
 
 		#if mail_send_request:
 		#	send_mail('solicitud de contacto', plain_message, settings.EMAIL_HOST_USER,
