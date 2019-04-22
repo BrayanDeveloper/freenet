37e8a20bd7431617402d099c9590cf386f912249
Brayan <ganbetacool@gmail.com>
2019-04-22 18:19:55 -0500

    se difundio el footer en las distintas paginas

--- apps/pagina/views.py
+++ apps/pagina/views.py
@@ -17,27 +17,30 @@ def index(request):
 def services(request):
 	if request.method == "GET":
 		statement_services_all = Service.objects.all()
+		data_footer = Footer.objects.all()
 		statement_services = Service_plus.objects.filter(id_service=request.GET.get('id'))
 		name = request.GET.get('name')
-		context = {'statement_services': statement_services, 'statement_services_all': statement_services_all, 'name':name}
+		context = {'statement_services': statement_services, 'statement_services_all': statement_services_all, 'name':name, 'data_footer':data_footer}
 		return render(request, 'blosure/services.html', context)
 	else:
 		statement_services_all = Service.objects.all()
 		statement_services = Service_plus.objects.all()
-		context = {'statement_services': statement_services, 'statement_services_all': statement_services_all}
+		data_footer = Footer.objects.all()
+		context = {'statement_services': statement_services, 'statement_services_all': statement_services_all, 'data_footer':data_footer}
 		return render(request, 'blosure/services.html', context)
 		
 
 def contact(request):
 	if request.method == "POST":
 		statement_services_all = Service.objects.all()
+		data_footer = Footer.objects.all()
 		statement = Contact(name=request.POST.get('name'), message=request.POST.get('message'), email=request.POST.get('email'),)
 		statement.save()
 
 		email = request.POST.get('email', None)
 		message = request.POST.get('message')
 		name = request.POST.get('name')
-		context = {'email': email, 'message': message, 'name':name, 'enviado':'enviado con exito', 'statement_services_all': statement_services_all}
+		context = {'email': email, 'message': message, 'name':name, 'enviado':'enviado con exito', 'statement_services_all': statement_services_all, 'data_footer':data_footer}
 
 		#html_message = render_to_string('template_email/email.html', {'context': context})
 		#plain_message = strip_tags(html_message)
@@ -54,11 +57,13 @@ def contact(request):
 
 		return render(request, 'blosure/contact.html', context)
 	statement_services_all = Service.objects.all()
-	return render(request, 'blosure/contact.html', {'statement_services_all': statement_services_all } )
+	data_footer = Footer.objects.all()
+	return render(request, 'blosure/contact.html', {'statement_services_all': statement_services_all, 'data_footer':data_footer } )
 
 def appointment(request):
 	if request.method == "POST":
 		statement_services_all = Service.objects.all()
+		data_footer = Footer.objects.all()
 		statement = Appointment(name=request.POST.get('name'), message=request.POST.get('message'), email=request.POST.get('email'), cel_phone=request.POST.get('phone'), dia=request.POST.get('dia'))
 		statement.save()
 
@@ -68,7 +73,7 @@ def appointment(request):
 		message = request.POST.get('message')
 		name = request.POST.get('name')
 		qqtitle = "Cita agendada por " + name + " a FreenetBusiness"
-		context = {'email': email, 'phone':phone, 'dia':dia, 'message': message, 'name':name, 'enviado':'Cita agendada exitosamente', 'statement_services_all': statement_services_all}
+		context = {'email': email, 'phone':phone, 'dia':dia, 'message': message, 'name':name, 'enviado':'Cita agendada exitosamente', 'statement_services_all': statement_services_all, 'data_footer':data_footer}
 
 		#html_message = render_to_string('template_email/mail_appointment.html', {'context': context})
 		#plain_message = strip_tags(html_message)
@@ -83,37 +88,43 @@ def appointment(request):
 
 		return render(request, 'blosure/appointment.html', context)
 	statement_services_all = Service.objects.all()
-	return render(request, 'blosure/appointment.html', {'statement_services_all': statement_services_all } )
+	data_footer = Footer.objects.all()
+	return render(request, 'blosure/appointment.html', {'statement_services_all': statement_services_all, 'data_footer':data_footer } )
 
 
 def about(request):
 	statement_services_all = Service.objects.all()
 	statement_about = About.objects.all()
-	context = {'statement_about': statement_about, 'statement_services_all': statement_services_all}
+	data_footer = Footer.objects.all()
+	context = {'statement_about': statement_about, 'statement_services_all': statement_services_all, 'data_footer':data_footer}
 	return render(request, 'blosure/about.html', context)
 
 
 def services_details(request):
 	if request.method == "GET":
 		statement_services_all = Service.objects.all()
+		data_footer = Footer.objects.all()
 		statement_services = Service_plus.objects.filter(id=request.GET.get('id'))
-		context = {'statement_services': statement_services, 'statement_services_all': statement_services_all}
+		context = {'statement_services': statement_services, 'statement_services_all': statement_services_all, 'data_footer':data_footer}
 		return render(request, 'blosure/services_details.html', context)
 
 def search_services(request):
 	if request.method == "POST":
 		statement_services_all = Service.objects.all()
+		data_footer = Footer.objects.all()
 		statement_services = Service_plus.objects.filter(name_service__contains=request.POST.get('search'), description_service__contains=request.POST.get('search'))
-		context = {'statement_services': statement_services, 'statement_services_all': statement_services_all}
+		context = {'statement_services': statement_services, 'statement_services_all': statement_services_all, 'data_footer':data_footer}
 		return render(request, 'blosure/search_services.html', context)
 	else:
 		statement_services_all = Service.objects.all()
 		statement_services = Service_plus.objects.all()
-		context = {'statement_services': statement_services, 'statement_services_all': statement_services_all}
+		data_footer = Footer.objects.all()
+		context = {'statement_services': statement_services, 'statement_services_all': statement_services_all, 'data_footer':data_footer}
 		return render(request, 'blosure/search_services.html', context)
 
 def ask_frecuent(request):
 	statement_services_all = Service.objects.all()
 	statement = Ask_frecuent.objects.all()
-	context = {'ask_frecuents':statement, 'statement_services_all':statement_services_all}
+	data_footer = Footer.objects.all()
+	context = {'ask_frecuents':statement, 'statement_services_all':statement_services_all, 'data_footer':data_footer}
 	return render(request, 'blosure/ask_frecuent.html', context)
