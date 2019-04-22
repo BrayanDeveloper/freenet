3c1ce75c17212c14bbbed4e6d598caad77f8af0b
Brayan <ganbetacool@gmail.com>
2019-04-22 18:14:10 -0500

    se agrego el tamaño de imagen para servicios dinamicamente

--- apps/pagina/models.py
+++ apps/pagina/models.py
@@ -18,6 +18,8 @@ class Service_plus(models.Model):
     description_service = models.CharField(max_length=9000)
     color_background = models.CharField(max_length=60)
     image_service = models.ImageField(upload_to='static/img/images_services/')
+    image_width = models.CharField(max_length=60)
+    image_height = models.CharField(max_length=60)
     link_service = models.CharField(max_length=255)
     price_service = models.CharField(max_length=255)
     developer_service = models.CharField(max_length=255)
